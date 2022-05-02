f6345f9fbb1f456c0884b1cc4e05afd950f0b81c
David Setim <setimdavid@gmail.com>
2022-05-02 14:03:19 +0300

    Merchant category api

---
+++ src/main/java/com/commerce/pal/backend/controller/merchant/MerchantCategoryController.java
@@ -0,0 +1,110 @@
+package com.commerce.pal.backend.controller.merchant;
+
+import com.commerce.pal.backend.common.ResponseCodes;
+import com.commerce.pal.backend.models.LoginValidation;
+import com.commerce.pal.backend.module.product.CategoryService;
+import com.commerce.pal.backend.repo.product.ProductRepository;
+import com.commerce.pal.backend.repo.user.MerchantRepository;
+import com.commerce.pal.backend.utils.GlobalMethods;
+import lombok.extern.java.Log;
+import org.json.JSONObject;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Log
+@CrossOrigin(origins = {"*"}, maxAge = 3600L)
+@RestController
+@RequestMapping({"/prime/api/v1/merchant/category"})
+@SuppressWarnings("Duplicates")
+public class MerchantCategoryController {
+    private final GlobalMethods globalMethods;
+    private final CategoryService categoryService;
+    private final ProductRepository productRepository;
+    private final MerchantRepository merchantRepository;
+
+    @Autowired
+    public MerchantCategoryController(GlobalMethods globalMethods,
+                                      CategoryService categoryService,
+                                      ProductRepository productRepository,
+                                      MerchantRepository merchantRepository) {
+        this.globalMethods = globalMethods;
+        this.categoryService = categoryService;
+        this.productRepository = productRepository;
+        this.merchantRepository = merchantRepository;
+    }
+
+    @RequestMapping(value = {"/get-parent-category"}, method = {RequestMethod.GET}, produces = {"application/json"})
+    @ResponseBody
+    public ResponseEntity<?> getParentCategory() {
+        JSONObject responseMap = new JSONObject();
+        LoginValidation user = globalMethods.fetchUserDetails();
+        merchantRepository.findMerchantByEmailAddress(user.getEmailAddress())
+                .ifPresentOrElse(merchant -> {
+                    List<JSONObject> data = new ArrayList<>();
+                    productRepository.findProductsByProductParentCateoryId(merchant.getMerchantId())
+                            .forEach(parentCategory -> {
+                                data.add(categoryService.getCategoryInfo(parentCategory));
+                            });
+                    responseMap.put("statusCode", ResponseCodes.SUCCESS)
+                            .put("statusDescription", "success")
+                            .put("data", data)
+                            .put("statusMessage", "Request Successful");
+                }, () -> {
+                    responseMap.put("statusCode", ResponseCodes.REQUEST_FAILED)
+                            .put("statusDescription", "Merchant Does not exists")
+                            .put("statusMessage", "Merchant Does not exists");
+                });
+        return ResponseEntity.ok(responseMap.toString());
+    }
+    @RequestMapping(value = {"/get-category"}, method = {RequestMethod.GET}, produces = {"application/json"})
+    @ResponseBody
+    public ResponseEntity<?> getCategory() {
+        JSONObject responseMap = new JSONObject();
+        LoginValidation user = globalMethods.fetchUserDetails();
+        merchantRepository.findMerchantByEmailAddress(user.getEmailAddress())
+                .ifPresentOrElse(merchant -> {
+                    List<JSONObject> data = new ArrayList<>();
+                    productRepository.findProductsByProductCategoryId(merchant.getMerchantId())
+                            .forEach(parentCategory -> {
+                                data.add(categoryService.getCategoryInfo(parentCategory));
+                            });
+                    responseMap.put("statusCode", ResponseCodes.SUCCESS)
+                            .put("statusDescription", "success")
+                            .put("data", data)
+                            .put("statusMessage", "Request Successful");
+                }, () -> {
+                    responseMap.put("statusCode", ResponseCodes.REQUEST_FAILED)
+                            .put("statusDescription", "Merchant Does not exists")
+                            .put("statusMessage", "Merchant Does not exists");
+                });
+        return ResponseEntity.ok(responseMap.toString());
+    }
+
+    @RequestMapping(value = {"/get-sub-category"}, method = {RequestMethod.GET}, produces = {"application/json"})
+    @ResponseBody
+    public ResponseEntity<?> getSubCategory() {
+        JSONObject responseMap = new JSONObject();
+        LoginValidation user = globalMethods.fetchUserDetails();
+        merchantRepository.findMerchantByEmailAddress(user.getEmailAddress())
+                .ifPresentOrElse(merchant -> {
+                    List<JSONObject> data = new ArrayList<>();
+                    productRepository.findProductsByProductSubCategoryId(merchant.getMerchantId())
+                            .forEach(parentCategory -> {
+                                data.add(categoryService.getSubCategoryInfo(parentCategory));
+                            });
+                    responseMap.put("statusCode", ResponseCodes.SUCCESS)
+                            .put("statusDescription", "success")
+                            .put("data", data)
+                            .put("statusMessage", "Request Successful");
+                }, () -> {
+                    responseMap.put("statusCode", ResponseCodes.REQUEST_FAILED)
+                            .put("statusDescription", "Merchant Does not exists")
+                            .put("statusMessage", "Merchant Does not exists");
+                });
+        return ResponseEntity.ok(responseMap.toString());
+    }
+}
--- src/main/java/com/commerce/pal/backend/repo/product/ProductRepository.java
+++ src/main/java/com/commerce/pal/backend/repo/product/ProductRepository.java
@@ -2,6 +2,7 @@ package com.commerce.pal.backend.repo.product;
 
 import com.commerce.pal.backend.models.product.Product;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
 import java.util.List;
 import java.util.Optional;
@@ -13,4 +14,13 @@ public interface ProductRepository extends JpaRepository<Product, Long> {
     Optional<Product> findProductByOwnerTypeAndMerchantIdAndProductId(String type, Long merchant, Long product);
 
     List<Product> findProductsByProductParentCateoryIdAndProductCategoryIdAndProductSubCategoryId(Long par, Long cat, Long sub);
+
+    @Query(value = "SELECT ProductParentCateoryId  FROM Product WHERE MerchantId = ?1  GROUP BY ProductParentCateoryId", nativeQuery = true)
+    List<Long> findProductsByProductParentCateoryId(Long merchant);
+
+    @Query(value = "SELECT ProductCategoryId  FROM Product WHERE MerchantId = ?1  GROUP BY ProductCategoryId", nativeQuery = true)
+    List<Long> findProductsByProductCategoryId(Long merchant);
+
+    @Query(value = "SELECT ProductSubCategoryId  FROM Product WHERE MerchantId = ?1  GROUP BY ProductSubCategoryId", nativeQuery = true)
+    List<Long> findProductsByProductSubCategoryId(Long merchant);
 }
