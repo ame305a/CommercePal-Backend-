276893b235001766f551b8795159cd3ababd2b84
David Setim <setimdavid@gmail.com>
2022-06-22 11:36:48 +0300

    Updates

--- src/main/java/com/commerce/pal/backend/controller/business/BusinessShoppingController.java
+++ src/main/java/com/commerce/pal/backend/controller/business/BusinessShoppingController.java
@@ -2,7 +2,10 @@ package com.commerce.pal.backend.controller.business;
 
 import com.commerce.pal.backend.common.ResponseCodes;
 import com.commerce.pal.backend.models.LoginValidation;
+import com.commerce.pal.backend.module.product.CategoryService;
 import com.commerce.pal.backend.module.product.ProductService;
+import com.commerce.pal.backend.repo.product.categories.ProductCategoryRepository;
+import com.commerce.pal.backend.repo.product.categories.ProductSubCategoryRepository;
 import com.commerce.pal.backend.repo.user.BusinessRepository;
 import com.commerce.pal.backend.service.specification.SpecificationsDao;
 import com.commerce.pal.backend.service.specification.utils.SearchCriteria;
@@ -25,24 +28,91 @@ import java.util.Optional;
 public class BusinessShoppingController {
     private final GlobalMethods globalMethods;
     private final ProductService productService;
+    private final CategoryService categoryService;
     private final SpecificationsDao specificationsDao;
     private final BusinessRepository businessRepository;
+    private final ProductCategoryRepository productCategoryRepository;
+    private final ProductSubCategoryRepository productSubCategoryRepository;
 
     @Autowired
     public BusinessShoppingController(GlobalMethods globalMethods,
                                       ProductService productService,
+                                      CategoryService categoryService,
                                       SpecificationsDao specificationsDao,
-                                      BusinessRepository businessRepository) {
+                                      BusinessRepository businessRepository,
+                                      ProductCategoryRepository productCategoryRepository,
+                                      ProductSubCategoryRepository productSubCategoryRepository) {
         this.globalMethods = globalMethods;
         this.productService = productService;
+        this.categoryService = categoryService;
         this.specificationsDao = specificationsDao;
         this.businessRepository = businessRepository;
+        this.productCategoryRepository = productCategoryRepository;
+        this.productSubCategoryRepository = productSubCategoryRepository;
+    }
+
+    @RequestMapping(value = {"/get-categories"}, method = {RequestMethod.GET}, produces = {"application/json"})
+    @ResponseBody
+    public ResponseEntity<?> getCategories() {
+        JSONObject responseMap = new JSONObject();
+        List<JSONObject> details = new ArrayList<>();
+        LoginValidation user = globalMethods.fetchUserDetails();
+        businessRepository.findBusinessByEmailAddress(user.getEmailAddress())
+                .ifPresentOrElse(business -> {
+                    productCategoryRepository.findProductCategoriesByParentCategoryId(Long.valueOf(business.getBusinessSector()))
+                            .forEach(cat -> {
+                                JSONObject detail = categoryService.getCategoryInfo(cat.getId());
+                                details.add(detail);
+                            });
+                }, () -> {
+                    responseMap.put("statusCode", ResponseCodes.REQUEST_FAILED)
+                            .put("statusDescription", "Merchant Does not exists")
+                            .put("statusMessage", "Merchant Does not exists");
+                });
+
+        responseMap.put("statusCode", ResponseCodes.SUCCESS)
+                .put("statusDescription", "success")
+                .put("details", details)
+                .put("statusMessage", "Request Successful");
+        return ResponseEntity.ok(responseMap.toString());
+    }
+
+    @RequestMapping(value = {"/get-sub-categories"}, method = {RequestMethod.GET}, produces = {"application/json"})
+    @ResponseBody
+    public ResponseEntity<?> getSubCategories(@RequestParam("category") String category) {
+        JSONObject responseMap = new JSONObject();
+        List<JSONObject> details = new ArrayList<>();
+        LoginValidation user = globalMethods.fetchUserDetails();
+        businessRepository.findBusinessByEmailAddress(user.getEmailAddress())
+                .ifPresentOrElse(business -> {
+                    productCategoryRepository.findProductCategoryByIdAndParentCategoryId(
+                                    Long.valueOf(category), Long.valueOf(business.getBusinessSector()))
+                            .ifPresentOrElse(cat -> {
+                                productSubCategoryRepository.findProductSubCategoriesByProductCategoryId(Long.valueOf(category))
+                                        .forEach(subCat -> {
+                                            JSONObject detail = categoryService.getSubCategoryInfo(subCat.getId());
+                                            details.add(detail);
+                                        });
+                            }, () -> {
+                                responseMap.put("statusCode", ResponseCodes.REQUEST_FAILED)
+                                        .put("statusDescription", "Business Does not exists")
+                                        .put("statusMessage", "Business Does not exists");
+                            });
+                }, () -> {
+                    responseMap.put("statusCode", ResponseCodes.REQUEST_FAILED)
+                            .put("statusDescription", "Business Does not exists")
+                            .put("statusMessage", "Business Does not exists");
+                });
+        responseMap.put("statusCode", ResponseCodes.SUCCESS)
+                .put("statusDescription", "success")
+                .put("details", details)
+                .put("statusMessage", "Request Successful");
+        return ResponseEntity.ok(responseMap.toString());
     }
 
     @RequestMapping(value = {"/get-products"}, method = {RequestMethod.GET}, produces = {"application/json"})
     @ResponseBody
-    public ResponseEntity<?> getProducts(@RequestParam("parent") Optional<String> parent,
-                                         @RequestParam("category ") Optional<String> category,
+    public ResponseEntity<?> getProducts(@RequestParam("category") Optional<String> category,
                                          @RequestParam("subCat") Optional<String> subCat,
                                          @RequestParam("brand") Optional<String> brand,
                                          @RequestParam("product") Optional<String> product) {
@@ -52,9 +122,6 @@ public class BusinessShoppingController {
         businessRepository.findBusinessByEmailAddress(user.getEmailAddress())
                 .ifPresentOrElse(business -> {
                     List<SearchCriteria> params = new ArrayList<SearchCriteria>();
-                    parent.ifPresent(value -> {
-                        params.add(new SearchCriteria("productParentCateoryId", ":", value));
-                    });
                     category.ifPresent(value -> {
                         params.add(new SearchCriteria("productCategoryId", ":", value));
                     });
@@ -68,7 +135,7 @@ public class BusinessShoppingController {
                         params.add(new SearchCriteria("productId", ":", value));
                     });
                     params.add(new SearchCriteria("status", ":", 1));
-                    params.add(new SearchCriteria("businessSector", ":", business.getBusinessSector()));
+                    params.add(new SearchCriteria("productParentCateoryId", ":", business.getBusinessSector()));
                     params.add(new SearchCriteria("productType", ":", "WHOLESALE"));
 
                     List<JSONObject> details = new ArrayList<>();
--- src/main/java/com/commerce/pal/backend/repo/product/categories/ProductCategoryRepository.java
+++ src/main/java/com/commerce/pal/backend/repo/product/categories/ProductCategoryRepository.java
@@ -4,8 +4,11 @@ import com.commerce.pal.backend.models.product.categories.ProductCategory;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.List;
+import java.util.Optional;
 
 public interface ProductCategoryRepository extends JpaRepository<ProductCategory, Long> {
 
     List<ProductCategory> findProductCategoriesByParentCategoryId(Long id);
+
+    Optional<ProductCategory> findProductCategoryByIdAndParentCategoryId(Long id, Long parent);
 }
